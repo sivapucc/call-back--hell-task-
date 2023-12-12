7d00917348bb6a78c14834cca7136a8082cc7cf0
sivapucc <124483364+sivapucc@users.noreply.github.com>
2023-12-12 17:16:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let ele = document.createElement("div");
+ele.setAttribute("class", "disp");
+document.body.append(ele);
+
+setTimeout(() => {
+    ele.innerHTML = "10";
+    ele.style.color = '#ffea00';
+    setTimeout(() => {
+        ele.innerHTML = "9";
+        ele.style.color = '#168aad';
+        setTimeout(() => {
+            ele.innerHTML = "8";
+            ele.style.color = '#fb8500';
+            setTimeout(() => {
+                ele.innerHTML = "7";
+                ele.style.color = '#147df5';
+                setTimeout(() => {
+                    ele.innerHTML = "6";
+                    ele.style.color = '#d62828';
+                    setTimeout(() => {
+                        ele.innerHTML = "5";
+                        ele.style.color = '#003049';
+                        setTimeout(() => {
+                            ele.innerHTML = "4";
+                            ele.style.color = '#5e548e';
+                            setTimeout(() => {
+                                ele.innerHTML = "3";
+                                ele.style.color = '#457b9d';
+                                setTimeout(() => {
+                                    ele.innerHTML = "2";
+                                    ele.style.color = '#38b000';
+                                    setTimeout(() => {
+                                        ele.innerHTML = "1";
+                                        ele.style.color = '#0077b6';
+                                        setTimeout(() => {
+                                            ele.innerHTML = "Happy Indipendence Day";
+                                            ele.style.color = '#ff006e';
+                                            ele.style.fontSize = '6rem';
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    },1000)
+},1000)
